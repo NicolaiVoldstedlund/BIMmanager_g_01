46fcffbbab4ad8f6a04f3c48114385e6b166663b
NicolaiVoldstedlund <s225332@dtu.dk>
2025-09-27 09:48:07 +0200

    Made a few changes so it works when importing

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 import ifcopenshell
 
-from external.BIManalyst_g_xy.rules import windowRule
-from external.BIManalyst_g_xy.rules import doorRule
-# from external.BIManalyst_g_xy.rules import xxxx
-# from external.BIManalyst_g_xy.rules import xxxx
-# from external.BIManalyst_g_xy.rules import xxxx
+from external.BIMANALYST_G_4.rules import FacadeTransparency
+from external.openBIM2025_GRP2.rules import alisaRule
 
-model = ifcopenshell.open(r"C:\Users\nicol\OneDrive - Danmarks Tekniske Universitet\DTU\7. Semester\41934 Advanced Building Information Modeling\GitHub\AdvancedBIM_ARCH\model\25-16-D-ARCH.ifc")
+model = ifcopenshell.open(
+    r"C:\Users\nicol\OneDrive - Danmarks Tekniske Universitet\DTU\7. Semester\41934 Advanced Building Information Modeling\GitHub\AdvancedBIM_ARCH\model\25-16-D-ARCH.ifc"
+)
 
-windowResult = windowRule.checkRule(model)
-doorResult = doorRule.checkRule(model)
+rules = {
+    "1": ("Facade Transparency", FacadeTransparency.checkRule),
+    "2": ("Alisa Rule", alisaRule.checkRule)
+}
 
-print("Window result:", windowResult)
-print("Door result:", doorResult)
+print("Vælg hvilken rule du vil køre:")
+for key, (name, _) in rules.items():
+    print(f"{key}: {name}")
+
+valg = input("Indtast nummeret på den ønskede rule: ")
+
+if valg in rules:
+    rule_name, rule_func = rules[valg]
+    result = rule_func(model)
+    print(f"{rule_name} result:", result)
+else:
+    print("Ugyldigt valg.")
